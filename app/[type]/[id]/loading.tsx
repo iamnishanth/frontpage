7124c8bb2a80072328b812eee24c1b879fc60de7
iamnishanth <nishanthvenk68@gmail.com>
2024-07-23 03:09:31 +0530

    fix: loading UI changes

--- app/[type]/[id]/loading.tsx
+++ app/[type]/[id]/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function PostLoading() {
   return (
-    <div className="flex-1 p-6 overflow-scroll">
+    <div className="flex-1 p-4 overflow-scroll">
       <Skeleton className="h-10 w-full mb-2" />
       <Skeleton className="h-10 w-1/2 mb-4" />
       <Skeleton className="h-5 w-full mb-2" />
--- components/posts-loading.tsx
+++ components/posts-loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "./ui/skeleton";
 
 export const PostsLoading = () => {
   return (
-    <div className="flex-1 p-6 overflow-scroll">
+    <div className="flex-1 p-2 lg:p-4 overflow-scroll">
       <Skeleton className="h-24 w-full mb-2 lg:mb-4" />
       <Skeleton className="h-24 w-full mb-2 lg:mb-4" />
       <Skeleton className="h-24 w-full mb-2 lg:mb-4" />
